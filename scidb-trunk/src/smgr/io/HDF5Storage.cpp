56a43d8141e8b81dd58db55bf89bca5f5a3249ed
Haoyuan Xing <hbsnmyj@gmail.com>
2016-05-22 21:47:23 -0400

    added hdf5 interface

---
+++ scidb-trunk/src/smgr/io/HDF5Storage.cpp
@@ -0,0 +1,82 @@
+#include "HDF5Storage.h"
+
+#include <algorithm>
+#include <functional>
+
+namespace scidb {
+namespace hdf5gateway
+{
+
+    HDF5File::HDF5File(std::string const &file, bool is_read)
+    {
+        if (is_read == false) {
+            _fileID = H5Fcreate(file.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+            assert(_fileID >= 0);
+        }
+    }
+
+    HDF5File::~HDF5File()
+    {
+        if (_fileID >= 0)
+            H5Fclose(_fileID);
+    }
+
+
+    HDF5Dataset::HDF5Dataset(HDF5File &file, std::string const &datasetName, std::string const& typeName,
+                                                 const scidb::hdf5gateway::H5Coordinates &dims,
+                                                 const scidb::hdf5gateway::H5Coordinates &chunk_dims) :
+            _stride(dims.size(), 1), _count(dims.size(), 1), _type(typeName)
+    {
+        hid_t file_id = file.getFileID();
+        auto rank = static_cast<int>(dims.size());
+        assert(dims.size() == chunk_dims.size());
+        hid_t dspace_id = H5Screate_simple(rank, dims.data(), NULL);
+        hid_t dcpl_id = H5Pcreate(H5P_DATASET_CREATE);
+        H5Pset_chunk(dcpl_id, rank, chunk_dims.data());
+
+        _datasetId = H5Dcreate(file_id, datasetName.c_str(), _type.getHDF5Type(), dspace_id,
+                H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
+        assert(_datasetId >= 0);
+        _fspaceId = H5Dget_space(_datasetId);
+        _mspaceId = H5Screate_simple(rank, dims.data(), NULL);
+    }
+
+    HDF5Dataset::~HDF5Dataset()
+    {
+        if (_datasetId >= 0)
+            H5Fclose(_datasetId);
+    }
+
+    int HDF5Dataset::writeData(void* data, H5Coordinates const& target_pos, H5Coordinates const& block)
+    {
+        H5Sselect_hyperslab(_fspaceId, H5S_SELECT_SET,
+                            target_pos.data(), _stride.data(), _count.data(), block.data());
+        H5Dwrite(_datasetId, _type.getHDF5Type(), _mspaceId, _fspaceId, H5P_DEFAULT, data);
+        return 0;
+    }
+
+    int HDF5Dataset::writeChunk(ConstChunk const& chunk, H5Coordinates const& target_pos)
+    {
+        void* data = chunk.getData();
+        auto first = chunk.getFirstPosition(false);
+        auto last = chunk.getLastPosition(false);
+        H5Coordinates block;
+        block.reserve(first.size());
+        std::transform(last.begin(), last.end(), first.begin(), std::back_inserter(block),
+                std::minus<size_t>());
+        writeData(data, target_pos, block);
+        return 0;
+    }
+
+    HDF5Type::HDF5Type(std::string const& typeName)
+    {
+        if(typeName == "float") {
+            _hdf5_type = H5T_NATIVE_FLOAT;
+        }
+    }
+
+}
+}
+
+
+
---
+++ scidb-trunk/src/smgr/io/HDF5Storage.h
@@ -0,0 +1,104 @@
+/*
+**
+* BEGIN_COPYRIGHT
+*
+* Copyright (C) 2008-2015 SciDB, Inc.
+* All Rights Reserved.
+*
+* SciDB is free software: you can redistribute it and/or modify
+* it under the terms of the AFFERO GNU General Public License as published by
+* the Free Software Foundation.
+*
+* SciDB is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
+* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+* NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
+* the AFFERO GNU General Public License for the complete license terms.
+*
+* You should have received a copy of the AFFERO GNU General Public License
+* along with SciDB.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
+*
+* END_COPYRIGHT
+*/
+
+/*
+ * @file HDF5Storage.h
+ *
+ *  Created on: 19.02.2010
+ *      Author: xing.136@osu.edu
+ * Description: HDF5-related storage implementations.
+ */
+
+#ifndef SCIDB_HDF5WRITER_H_
+#define SCIDB_HDF5WRITER_H_
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <hdf5.h>
+#include <array/Coordinate.h>
+#include <array/Array.h>
+
+namespace scidb {
+namespace hdf5gateway {
+
+    using H5Coordinates = std::vector<hsize_t>;
+
+    /**
+     * a class to represent the type in HDF5 file.
+     */
+    class HDF5Type {
+      public:
+        HDF5Type(std::string const& typeName);
+        hid_t getHDF5Type() { return _hdf5_type; }
+      private:
+        hid_t _hdf5_type;
+    };
+
+    class HDF5File {
+      public:
+        HDF5File(std::string const& file, bool is_read);
+        HDF5File(const HDF5File&) = delete;
+        HDF5File(HDF5File&&) = delete;
+        HDF5File& operator=(const HDF5File&) = delete;
+        HDF5File& operator=(HDF5File&&) = delete;
+        ~HDF5File();
+        hid_t getFileID() { return _fileID; }
+
+      private:
+        hid_t _fileID;
+    };
+
+    class HDF5Dataset {
+      public:
+        /*
+         * create dataset in hdf5 file
+         * @param file file to be created
+         * @param datasetName dataset name
+         * @param type the type of the dataset
+         * @param dims the total dimension of the dataset
+         * @param chunk dimension of the dataset
+         */
+        HDF5Dataset(HDF5File& file, std::string const& datasetName, std::string const& type,
+                    H5Coordinates const & dims, H5Coordinates const& chunk_dims);
+        HDF5Dataset(const HDF5Dataset&) = delete;
+        HDF5Dataset(HDF5Dataset&&) = delete;
+        HDF5Dataset& operator=(const HDF5Dataset&) = delete;
+        HDF5Dataset& operator=(HDF5Dataset&&) = delete;
+        ~HDF5Dataset();
+
+        int writeData(void* data, H5Coordinates const& target_pos, H5Coordinates const& block);
+        int writeChunk(ConstChunk const& chunk, H5Coordinates const& target_pos);
+      private:
+        hid_t _datasetId;
+        hid_t _fspaceId;
+        hid_t _mspaceId;
+        H5Coordinates _stride;
+        H5Coordinates _count;
+        HDF5Type _type;
+    };
+
+}
+}
+
+
+#endif //SCIDB_HDF5WRITER_H
